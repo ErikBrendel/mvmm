44b97a00baef5e9e3123b47b73c1fd6ce5da0016
Erik Brendel <erik.brendel@student.hpi.de>
2021-07-10 16:58:06 +0200

    evo main for testing

---
+++ scripts/evo_main.py
@@ -0,0 +1,16 @@
+import logging; logging.basicConfig(level=logging.INFO)
+import pdb
+import pyfiglet
+
+from custom_types import *
+from local_repo import *
+from repos import *
+from metrics import *
+from analysis import *
+
+r = LocalRepo("jfree/jfreechart")
+print(pyfiglet.figlet_format(r.name))
+
+
+MetricManager.clear(r, "evolutionary")
+MetricManager.get(r, "evolutionary").print_statistics()
