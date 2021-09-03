9b9b4edb70d354494a525d3bb0582424856757ec
Erik Brendel <erik.brendel@student.hpi.de>
2021-09-03 18:40:49 +0200

    highlighting the other method in the study code / small other changes

--- scripts/study_notebook_helper.py
+++ scripts/study_notebook_helper.py
@@ -1,4 +1,19 @@
 from local_repo import *
+import re
+
+
+def format_code(content: str, other_path: str) -> str:
+    other_path_parts = other_path.split("/")
+    if not any(part.endswith(".java") for part in other_path_parts):
+        other_path_parts = [other_path_parts[-1]]
+    else:
+        while any(part.endswith(".java") for part in other_path_parts):
+            other_path_parts = other_path_parts[1:]
+    content_no_html = content.replace("<", "&lt;").replace(">", "&gt;")
+    # language=HTML
+    replacement = r'<mark style="background-color: #b28549">\1</mark>'
+    return re.sub(fr"""\b({"|".join(other_path_parts)})\b""", replacement, content_no_html)
+
 
 # language=HTML
 OWN_STYLE = """
@@ -36,9 +51,10 @@ PRISM_HIGHLIGHTING = f"""
 {get_from_web("https://raw.githubusercontent.com/PrismJS/prism-themes/master/themes/prism-darcula.css")}
 </style>
 <script async>
-{get_from_web("https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.0//components/prism-core.min.js")}
-{get_from_web("https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.0//components/prism-clike.min.js")}
-{get_from_web("https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.0//components/prism-java.min.js")}
+{get_from_web("https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.0/components/prism-core.min.js")}
+{get_from_web("https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.0/components/prism-clike.min.js")}
+{get_from_web("https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.0/components/prism-java.min.js")}
+{get_from_web("https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.0/plugins/keep-markup/prism-keep-markup.min.js")}
 Prism.highlightAll();
 //setTimeout(() => Prism.highlightAll(), 1000);
 </script>
