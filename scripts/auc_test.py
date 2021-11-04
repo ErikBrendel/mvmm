62e33664de80303de9f09afeba3446323d5c2489
Erik Brendel <erik.brendel@student.hpi.de>
2021-11-04 09:58:37 +0100

    reorganive PRC example

--- scripts/auc_test.py
+++ scripts/auc_test.py
@@ -1,18 +1,31 @@
-# precision-recall curve and f1
+from typing import *
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_recall_curve
-from sklearn.metrics import f1_score
 from sklearn.metrics import auc
 from matplotlib import pyplot as plt
 
+
 # draw PR-diagram and calculate AUC
 # https://machinelearningmastery.com/roc-curves-and-precision-recall-curves-for-classification-in-python/
+def make_prc_plot(probabilities: List[float], actual_labels: List[int], label: str):
+    precision, recall, _ = precision_recall_curve(actual_labels, probabilities)
+    auc_value = auc(recall, precision)
+    no_skill = sum(actual_labels) / len(actual_labels)  # = P / total
+    plt.plot([0, 1], [no_skill, no_skill], linestyle='--', label='No Skill: ' + str(no_skill))
+    plt.plot(recall, precision, marker='.', label=label)
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.legend()
+    plt.ylim([no_skill - 0.03, 1.03])
+    plt.text(0.5, 0.3, f"AUC: {int(auc_value * 1000)/10}%", horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes)
+    plt.show()
 
-for n in [1000, 10000, 100000, 1000000]:
+
+if __name__ == "__main__":
     # generate 2 class dataset
-    X, y = make_classification(n_samples=n, n_classes=2, random_state=1)
+    X, y = make_classification(n_samples=1000000, n_classes=2, random_state=1, weights=(0.9,))
     # split into train/test sets
     trainX, testX, trainy, testy = train_test_split(X, y, test_size=0.5, random_state=2)
     # fit a model
@@ -22,21 +35,4 @@ for n in [1000, 10000, 100000, 1000000]:
     lr_probs = model.predict_proba(testX)
     # keep probabilities for the positive outcome only
     lr_probs = lr_probs[:, 1]
-    # predict class values
-    yhat = model.predict(testX)
-    lr_precision, lr_recall, _ = precision_recall_curve(testy, lr_probs)
-    lr_f1, lr_auc = f1_score(testy, yhat), auc(lr_recall, lr_precision)
-    # summarize scores
-    print('Logistic: f1=%.3f auc=%.3f' % (lr_f1, lr_auc))
-    # plot the precision-recall curves
-    no_skill = len(testy[testy == 1]) / len(testy)
-    plt.plot([0, 1], [no_skill, no_skill], linestyle='--', label='No Skill: ' + str(no_skill))
-    plt.plot(lr_recall, lr_precision, marker='.', label='Logistic')
-    # axis labels
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    # show the legend
-    plt.legend()
-    # show the plot
-    plt.ylim([0.47, 1.03])
-    plt.show()
+    make_prc_plot([p for p in lr_probs], [c for c in testy], "Example Curve")
